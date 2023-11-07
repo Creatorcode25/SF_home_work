84723881c08170a0e366fd784da59ffeddf8fa02
Creatorcode25 <e89915657991.smirnov@yandex.ru>
2023-11-07 13:51:02 +0300

    Initial commit

---
+++ game_core_v3.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+    count = 0
+    predict = np.random.randint(1, 101)
+    # print('Before predict = ', predict)
+    if predict > number:
+        d = predict
+        while predict != number:
+            count += 1
+            if predict > number and d != 1:
+                d = d//2
+                predict -= d
+            elif predict < number and d != 1:
+                d = d//2
+                predict += d
+            elif predict > number and d == 1:
+                predict -= 1
+            elif predict < number and d == 1:
+                predict += 1
+    else:
+        d1 = 100
+        while predict != number:
+            # print('After predict 2 = ', predict, 'd1 = ', d1)
+            count += 1
+            if predict < number and d1 != 1:
+                # print('After predict 3 = ', predict, 'd1 = ', d1)
+                d1 = d1//2
+                predict += d1                    
+            elif predict > number and d1 != 1:
+                # print('After predict 4 = ', predict, 'd1 = ', d1)
+                d1 = d1//2
+                predict -= d1
+            elif predict > number and d1 == 1:
+                # print('After predict 5 = ', predict, 'd1 = ', d1)
+                predict -= 1
+            elif predict < number and d1 == 1:
+                # print('After predict 6 = ', predict, 'd1 = ', d1)
+                predict += 1
+                
+    return count
+    # Ваш код заканчивается здесь
+print(score_game(game_core_v3))
+ 
